e52afe2f6b97391e640c3b67de4d7f55be482a9e
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-12-02 19:19:33 +0500

    feat: rewrite resources

--- resources/store.py
+++ resources/store.py
@@ -1,8 +1,9 @@
-import uuid
-
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
+from db import db
+from models import StoreModel
 from schemas import StoreSchema
 
 blp = Blueprint("stores", __name__, description="Operations on stores")
@@ -12,34 +13,35 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
-        try:
-            return stores[store_id]
-        except KeyError:
-            abort(404, massage='Store not found')
+        store = StoreModel.query.get_or_404(store_id)
+        return store
 
     def delete(self, store_id):
-        try:
-            del stores[store_id]
-            return {'massage': 'Store deleted'}
-        except KeyError:
-            abort(404, massage='Store not found')
+        store = StoreModel.query.get_or_404(store_id)
+        db.session.delete(store)
+        db.session.commit()
+        return {'massage': 'Store deleted'}
 
 
 @blp.route('/store')
 class StoreList(MethodView):
     @blp.response(200, StoreSchema(many=True))
     def get(self):
-        return stores.values()
+        return StoreModel.query.all()
 
     @blp.arguments(StoreSchema)
     @blp.response(201, StoreSchema)
     def post(self, store_data):
-        for store in stores.values():
-            if store_data["name"] == store["name"]:
-                abort(400, message=f"Store already exists.")
-
-        store_id = uuid.uuid4().hex
-        store = {**store_data, "id": store_id}
-        stores[store_id] = store
+        store = StoreModel(**store_data)
+        try:
+            db.session.add(store)
+            db.session.commit()
+        except IntegrityError:
+            abort(
+                400,
+                massage='The store with that name already exists'
+            )
+        except SQLAlchemyError:
+            abort(500, massage='An error occurred')
 
         return store
