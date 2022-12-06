ea55071d988c2ec9e8fe6cdba995c0cfe5acabfd
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-12-06 14:50:33 +0500

    feat: add tags to store

--- models/store.py
+++ models/store.py
@@ -9,3 +9,4 @@ class StoreModel(db.Model):
     items = db.relationship(
         'ItemModel', back_populates='store', lazy='dynamic'
     )
+    tags = db.relationship('TagModel', back_populates='store', lazy='dynamic')
